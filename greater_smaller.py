94c9fd76c71306c1c68db999517ba80a98d4c047
25Kpatel <patel.krish2407@gmail.com>
2026-03-11 18:35:20 +0000

    Add files via upload

---
+++ greater_smaller.py
@@ -0,0 +1,7 @@
+i=int(input("enter your number"))
+if i>100:
+    print("number is greater than hundred")
+    print("i am in if block")
+else:
+    print("number is smaller than 100")
+    print("i am in else block")
