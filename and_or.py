7d891f8c16dbf7a6001901de318b0cce73e03ff9
25Kpatel <patel.krish2407@gmail.com>
2026-03-18 18:38:35 +0000

    Add files via upload

---
+++ and_or.py
@@ -0,0 +1,19 @@
+a=10
+b=12
+c=0
+
+if a and b and c:
+
+    print("all the numbers have boolean values as true")
+
+else:
+    print("atleast number has boolean value as false")
+
+x=10
+y=-10
+z=0
+if x>0 or y>0:
+    print("either of the numbers are greater than 0")
+
+else:
+    print("no number is greater than 0")
