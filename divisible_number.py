8f1f969cad4aa440330bbcecd18f4192e09e5715
25Kpatel <patel.krish2407@gmail.com>
2026-04-02 14:46:35 +0100

    Add files via upload

---
+++ divisible_number.py
@@ -0,0 +1,6 @@
+numerator=int(input("enter your numerator"))
+denominator=int(input("enter your denominator"))
+if numerator%denominator==0:
+    print("number is divisible")
+else:
+    print("number is not divisible")
